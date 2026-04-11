e176b1772b050228142c8344dd9e3d884f626034
EC2 Default User <mmurawski777@gmail.com>
2026-04-11 17:22:02 +0000

    .

---
+++ run.sh
@@ -0,0 +1,3 @@
+export DOCKER_HOST=unix:///var/run/docker.sock
+set -e
+for i in {1..15}; do echo "--- Starting Run $i ---" | tee -a output.log; python3 -u agent/episodes_runner/experiment_runner.py 2>&1 | tee -a output.log; done
