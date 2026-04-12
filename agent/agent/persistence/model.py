2ab556570a0d4bd6a8788555beeb42a7091361f8
Michał Murawski <mmurawski777@gmail.com>
2026-04-12 19:04:00 +0200

    .

--- agent/agent/persistence/model.py
+++ agent/agent/persistence/model.py
@@ -25,7 +25,7 @@ class TaskModel(Model):
     last_message_ts = IntegerField()
     created_at = DateTimeField(default=datetime.now)
     updated_at = DateTimeField(default=datetime.now)
-    resolved_at = DateTimeField(default=None)
+    resolved_at = DateTimeField(null=True)
 
     class Meta:
         table_name = "tasks"
